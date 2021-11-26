d2eae65cff4b880e904df7fecd549881ff2713e7
kethavathsundar <90022710+kethavathsundar@users.noreply.github.com>
2021-11-26 21:46:16 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,11 @@
+function person(FirstName,LastName,Office_Address){
+this.FirstName=FirstName;
+this.LastName=LastName;
+this.Office_Address=Office_Address;
+}
+let p1=new person("Sundar","Kethavath","C-67,Hanuman Nagar,Devarakonda");
+let p2=new person("Raju","Chokka","Sagar(back water),Vizag colony,Nalgonda");
+let p3=new person("Amarthya","Choula","Road n0:4 Kakatiya colony,Hanmakonda,Warangal");
+console.log(p1);
+console.log(p2);
+console.log(p3);
